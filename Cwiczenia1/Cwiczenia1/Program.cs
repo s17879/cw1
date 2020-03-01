cf482b7fc3b998f0de692cff209964e7635bfb39
Sonia Wronkowska <s17879@pjwstk.edu.pl>
2020-03-01 12:26:52 +0100

    Dodany nowy projekt

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
