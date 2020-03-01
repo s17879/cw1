847010308342600f669180e227049214b5e632a8
Sonia Wronkowska <s17879@pjwstk.edu.pl>
2020-03-01 12:53:09 +0100

    Dokonczenie aplikacji

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenia1
 {
-    class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+
+            HttpResponseMessage result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+",
+                    RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+            }
+
+            Console.WriteLine("Koniec!");
         }
     }
 }
